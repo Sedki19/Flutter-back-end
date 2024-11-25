2b5414d7c0da6b0b78a9017c5f8cff3a7643e285
Sedki7 <sedkishabou7@gmail.com>
2024-11-25 21:56:14 +0100

    commit

---
+++ src/main/java/org/isetn/repository/MatiereRepository.java
@@ -0,0 +1,17 @@
+package org.isetn.repository;
+
+import java.util.List;
+
+import org.isetn.entities.Classe;
+import org.isetn.entities.Etudiant;
+import org.isetn.entities.Matiere;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+public interface MatiereRepository extends JpaRepository<Matiere, Long> {
+
+	
+	
+}
